5d03aea0d249c585319560ee14b6e1f706133022
GSlege <91965195+gslege@users.noreply.github.com>
2025-10-24 15:39:24 +0800

    Update JP.py

--- JP.py
+++ JP.py
@@ -266,7 +266,7 @@ class CloudflareNodeTester:
                 for i, node in enumerate(top_results):
                     # 获取IP的国家信息（已经是中文）
                     country = get_ip_country(node['ip'])
-                    line = f"{node['ip']}:{TEST_PORT}JP {node['response_time_ms']}ms\n"
+                    line = f"{node['ip']}jp 日本 JP {node['response_time_ms']}ms\n"
                     f.write(line)
             
         except Exception as e:
