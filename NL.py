b7403984453a0603038d9a0abb12e19359e72b2d
GSlege <91965195+gslege@users.noreply.github.com>
2025-10-25 19:06:04 +0800

    Update and rename NL.txt to NL.py

--- NL.py
+++ NL.py
@@ -151,7 +151,8 @@ class CloudflareNodeTester:
         
         # 常见的Cloudflare IP段
         ip_ranges = [
-"172.64.229.0/22"
+"104.20.0.0/22",
+"188.114.96.0/22"
         ]
         
         # 从IP段生成部分IP示例
