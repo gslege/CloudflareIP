7f7e56280585b04b0dfcf66f87be608e2118b47e
GSlege <91965195+gslege@users.noreply.github.com>
2025-10-26 18:24:28 +0800

    Update DE.py

--- DE.py
+++ DE.py
@@ -255,7 +255,7 @@ class CloudflareNodeTester:
         # 显示前N个最快节点，包含中文国家信息
         for i, node in enumerate(sorted_nodes[:TOP_NODES], 1):
             country = get_ip_country(node['ip'])
-            print(f"{node['ip']}#de 德国 JP")
+            print(f"{node['ip']}#de 德国 DE")
         
         return sorted_nodes
     
