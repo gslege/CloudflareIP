18de8c078b3ea4a3f884ec87a775d3fc0f619be2
a2831679378-cmyk <a2831679378@gmail.com>
2026-02-02 16:12:16 +0800

    Change UUID for 我的VL密钥
    Updated the UUID for 我的VL密钥.

--- CF-Worker/_worker.js
+++ CF-Worker/_worker.js
@@ -6,7 +6,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let 我的VL密钥 = '04c808e2-0b59-47b0-a54b-32fc7ef1c902';//UUID
+let 我的VL密钥 = 'd4f9d75e-7be7-4621-ba0e-61f04f10217a';//UUID
 let 反代IP = 'proxyip.cmliussss.net'; //反代IP
 
 export default {
